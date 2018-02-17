8f3e3f11b71562c4febce93f79e3a06cc652adb0
Marvin Filus <marvinfi@Marvins-MacBook-Air.local>
2018-02-16 19:04:11 -0500

    more animations

---
+++ open.js
@@ -0,0 +1,15 @@
+$('.wid').hide();
+// // There until I can fix the main
+// $('.opening').hide();
+
+
+$(document).ready(setTimeout(function(){
+	$('.begin').hide();
+	$('.wid').show();
+}, 5000));
+
+$(document).ready(setTimeout(function(){
+	$('.opening').hide();
+	$('.main').show();
+}, 10000));
+
