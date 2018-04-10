d07831ea25559f3f4fc3c13176f253e545822578
Marvin Filus <marvinfi@Marvins-Air.attlocal.net>
2018-04-09 20:02:05 -0400

    almost done

--- open.js
+++ open.js
@@ -1,22 +1,25 @@
 $('.wid').hide();
 $('#contact-me').hide();
 // There until I can fix the main
-$('.opening').hide();
+// $('.opening').hide();
 
 
-// $(document).ready(setTimeout(function(){
-// 	$('.begin').hide();
-// 	$('.wid').show();
-// }, 5000));
+$(document).ready(setTimeout(function(){
+	$('.begin').hide();
+	$('.wid').show();
+}, 5000));
 
-// $(document).ready(setTimeout(function(){
-	// $('.opening').hide();
+$(document).ready(setTimeout(function(){
+	$('.opening').hide();
 	$('.main').show();
-// }, 10000));
+}, 10000));
 
 // This is to make the Contact slide in 
 $('#contact').on('click',function (event) {
 	event.preventDefault();
+	$(window).scrollTop(0);
+	$('.main').css('z-index',' 1 ');
+	$('#contact-me').css('z-index','2');
 	$('#contact-me').show();
 	hideMain();
 });
@@ -36,6 +39,8 @@ function hideContact(){
 $('#main').on('click',function(event){
 	event.preventDefault();
 	// $('.main').addClass('mainSlide');
+	$('.main').css('z-index',' 2 ');
+	$('#contact-me').css('z-index','1');
 	$('.main').show();
 	hideContact()
 })
